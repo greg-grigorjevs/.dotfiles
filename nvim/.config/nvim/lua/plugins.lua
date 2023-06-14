4d4bf90f3c921029cda36c31b8776d304814fa68
greg-grigorjevs <greg.grigorjevs@fodmobilitygroup.com>
2023-06-14 09:39:50 +0100

    new plugins + additional config for vim_bookmarks

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -70,7 +70,7 @@ return require('packer').startup(function()
       require('lightspeed').opts.ignore_case = true
     end
   }
-  
+
   use 'ggandor/leap.nvim'
 
   use {
@@ -216,6 +216,15 @@ return require('packer').startup(function()
 
   use 'tpope/vim-repeat' -- dot-repeat for some plugins
   use 'MattesGroeger/vim-bookmarks'
-  use 'tom-anders/telescope-vim-bookmarks.nvim'
+  use { 'tom-anders/telescope-vim-bookmarks.nvim',
+    config = function()
+      vim.g.bookmark_save_per_working_dir = 0
+      vim.g.bookmark_auto_save = 1
+    end
+  }
+
+  use 'tpope/vim-fugitive'
+  use 'nvim-treesitter/nvim-treesitter-context'
+  use "b0o/schemastore.nvim"
 
 end)
