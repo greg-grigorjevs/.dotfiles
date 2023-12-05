9e7702934de7055571c79c79903e95b5d88770ae
Greg Grigorjevs <ggrigorjevs@icloud.com>
2023-12-05 11:51:14 +0000

    minor changes

---
+++ bin/stow_script.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+if [[ -z $STOW_FOLDERS ]]; then
+    STOW_FOLDERS="nvim,tmux,scripts,kitty,fzf,lazygit,zsh,karabiner"
+fi
+
+if [[ -z $DOTFILES ]]; then
+    DOTFILES=$HOME/.dotfiles
+fi
+
+pushd $DOTFILES
+for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
+do
+    echo "stow $folder"
+    stow -D $folder
+    stow $folder
+done
+popd
+
