70efded9a298e8f785a29be3bc14e5447e924445
Greg Grigorjevs <ggrigorjevs@icloud.com>
2025-03-25 10:47:23 +0000

    hammerspoon: cleanup

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -22,35 +22,8 @@ hs.hotkey.bind({ "cmd", "shift" }, "7", function()
   hs.task.new('/usr/sbin/screencapture', nil, { '-l' .. windowId, '-c' }):start()
 end)
 
-
 require('window-management')
 require('screen1-switcher')
-
--- enable/disable kmonad when connecting/disconnecting my keyboards
-hs.usb.watcher.new(function(data)
-  if (data.productName == "Voyager" or data.productName == "Moonlander Mark I") then
-    if (data.eventType == "removed") then
-      hs.execute('kmonadon', true)
-    elseif (data.eventType == "added") then
-      hs.execute('kmonadoff', true)
-    end
-  end
-end)
---:start()
-
-
-
--- launches the app but doesn't switch between opened windows
--- TODO: add launching functionality to AppWindowSwitcher spoon
--- spoon.SpoonInstall:andUse('AppLauncher', {
---   config = {
---     modifiers = hyper
---   },
---   hotkeys = {
---     c = "Microsoft Teams",
---     -- g = "Google Chrome"
---   }
--- })
 
 spoon.SpoonInstall:andUse('ReloadConfiguration', {
   config = {
