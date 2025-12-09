77a888aa59eb0749165c40cd50a414792d5b3491
Greg Grigorjevs <ggrigorjevs@icloud.com>
2025-12-09 12:56:51 +0800

    nvim-conform: fix tailwind sorting for blade formatter, add disable autoformat for a buffer and a keymap to toggle it

--- nvim/.config/nvim/lua/plugin_configs/conform.lua
+++ nvim/.config/nvim/lua/plugin_configs/conform.lua
@@ -3,19 +3,30 @@ return {
   config = function()
     local conform = require('conform')
     conform.setup({
+      formatters = {
+        ['blade-formatter'] = {
+          append_args = { '--sort-html-attributes=code-guide', '--sort-tailwindcss-classes' }
+        }
+      },
       formatters_by_ft = {
-        -- blade = { { 'prettierd', 'prettier' } },
         blade = { 'blade-formatter' },
         nix = { 'nixpkgs_fmt' },
         html = { 'prettierd' },
         javascript = { 'prettierd' }
       },
-      format_on_save = {
-        -- I recommend these options. See :help conform.format for details.
-        lsp_format = "fallback",
-        timeout_ms = 3000,
-      },
+      format_on_save = function()
+        if vim.b.disable_autoformat == true then
+          return
+        end
+
+        return { lsp_format = "fallback", timeout_ms = 3000 }
+      end
     })
+
+    -- Keymaps
+    vim.keymap.set({ "n" }, "<leader>hd", function()
+      vim.b.disable_autoformat = not vim.b.disable_autoformat
+    end, { desc = "Toggle buffer autoformat", noremap = true })
 
     vim.keymap.set({ "n", "v" }, "<leader>hf", function()
       conform.format({
